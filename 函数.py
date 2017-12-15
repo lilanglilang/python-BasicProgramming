d582a4568e1c9da5d9869afef3cfd194c506aa48
lilang <11170370113@qq.com>
2017-12-15 09:35:07 +0800

    函数问题

---
+++ 函数.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+name=('lilang','lilanbg')
+name=['baidu','hello']
+print(name)
+name[0]='google'
+print(name)
+print("nifasdf")
+strl="fsdafsadfs"
+print(strl[0])
+strl[0]='fadsfsad'
+
+def function():
+    globals()
