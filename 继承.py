b3a79402b137131f9b482c22fc8143b72c64cf9d
lilang <11170370113@qq.com>
2018-01-14 21:30:56 +0800

    继承

---
+++ 继承.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*
+class A(object):
+    def __init__(self):
+        self.name='helo'
+        self.__age='23'
+        self._sex='男'
+    def printHello(self):
+        print("hello")
+class B(A):
+    def haha(self):
+        super().printHello()
+b=B()
+print(b.name)
+print(b._sex)
+# print(b.__age) 私有属性不允许使用
+'''
+helo
+男
+object has no attribute '__age'
+'''
+# class C(A,B):
+#   pass
+# print(C.__mro__)
+# '''
+# TypeError: Cannot create a consistent method resolution
+# order (MRO) for bases A, B
+# '''
+class C(B,A):
+  pass
+print(C.__mro__)
+'''
+(<class '__main__.C'>, <class '__main__.B'>, <class '__main__.A'>, <class 'object'>)
+'''
