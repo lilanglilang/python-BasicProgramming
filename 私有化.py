21046d7d14e2cf1a727067618c9cf26faa75d34e
lilang <11170370113@qq.com>
2018-01-14 20:13:16 +0800

    私有化

--- 私有化.py
+++ 私有化.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-class person(object):
-    def _hello(self):
-        print("huanren")
 '''
 单前置下划线表示保护属性或者方法 from somemodel import * 禁止导入
 类对象和子类可以访问
 后置单下划线区分变量名
+双前下划线表示私有方法
 '''
+
+
+class person(object):
+    def _hello(self):
+        print("huanren")
+
+
 lilang=person()
 lilang._hello() #可以访问
 class Man(person):
